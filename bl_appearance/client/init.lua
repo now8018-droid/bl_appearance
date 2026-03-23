ec8f43811a975dcaa4f5c6677d0ad8875402ef5b
now8018-droid <now8018@gmail.com>
2026-03-23 11:54:57 +0700

    Limit heritage model selection to sex

--- bl_appearance/client/init.lua
+++ bl_appearance/client/init.lua
@@ -407,12 +407,29 @@ local function ensureAppearanceState()
     return currentAppearanceState
 end
 
+local sexModels = {
+    Male = 'mp_m_freemode_01',
+    Female = 'mp_f_freemode_01'
+}
+
+local function getSexModelOptions()
+    return { 'Male', 'Female' }
+end
+
+local function resolveSexModel(model)
+    if type(model) == 'string' and sexModels[model] then
+        return GetHashKey(sexModels[model])
+    end
+
+    return type(model) == 'number' and model or GetHashKey(model)
+end
+
 local function findModelIndex(target)
-    local models = exports.bl_appearance:models()
-    for i = 1, #models do
-        if GetHashKey(models[i]) == target then
-            return i - 1
-        end
+    if target == GetHashKey(sexModels.Male) then
+        return 0
+    end
+    if target == GetHashKey(sexModels.Female) then
+        return 1
     end
     return -1
 end
@@ -721,7 +738,7 @@ local function setModel(pedHandle, data)
     if data == nil then return pedHandle end
     local model
     if type(data) == 'string' then
-        model = GetHashKey(data)
+        model = resolveSexModel(data)
     elseif type(data) == 'number' then
         model = data
     elseif type(data) == 'table' then
@@ -923,7 +940,7 @@ RegisterNUICallback('appearance:save', function(appearance, cb)
 end)
 
 RegisterNUICallback('appearance:setModel', function(model, cb)
-    local hash = GetHashKey(model)
+    local hash = resolveSexModel(model)
     if not IsModelInCdimage(hash) or not IsModelValid(hash) then
         cb(0)
         return
@@ -1116,7 +1133,7 @@ local function openMenu(zone, creation)
     local allowExit = creation and false or menu.allowExit
     armour = GetPedArmour(pedHandle)
     local outfits = hasValue(tabs, 'outfits') and triggerServerCallback('bl_appearance:server:getOutfits', frameworkID) or nil
-    local models = hasValue(tabs, 'heritage') and getAllowlist(exports.bl_appearance:models()) or nil
+    local models = hasValue(tabs, 'heritage') and getSexModelOptions() or nil
     local tattoos = hasValue(tabs, 'tattoos') and getTattooData() or nil
     local blacklist = getBlacklist(zone)
 
